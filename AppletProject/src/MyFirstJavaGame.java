f4a96fb66d2ffe0d85b6b1b37faa698971260f50
dlagazo <dlagazo@gmail.com>
2014-12-12 11:58:20 +0800

    Added code and comments to main applet

--- AppletProject/src/MyFirstJavaGame.java
+++ AppletProject/src/MyFirstJavaGame.java
@@ -2,10 +2,23 @@ import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Graphics;
 
+/***
+ * 
+ * @author Daniel Lagazo
+ * @version 0.1
+ * @since December 12, 2014
+ * This will be our project for CIE 123. We will build a game incrementally.
+ * We will add characters and artificial intelligence. By the end of the course,
+ * you will have developed a full-blown, fully-documented game.
+ */
+
 
 public class MyFirstJavaGame extends Applet {
+	
+	
 	public void paint (Graphics g)
 	   {
+		
 			int width = getWidth();
 			int height = getHeight();
 			
