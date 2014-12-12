7750e32b155819ba357d916ddcef2642447188cc
dlagazo <dlagazo@gmail.com>
2014-12-12 11:37:17 +0800

    Main Applet

--- AppletProject/src/MyFirstJavaGame.java
+++ AppletProject/src/MyFirstJavaGame.java
@@ -6,9 +6,10 @@ import java.awt.Graphics;
 public class MyFirstJavaGame extends Applet {
 	public void paint (Graphics g)
 	   {
-	      g.drawString ("Hello World", 50, 50);
-	      g.setColor(Color.blue);
-	      g.fillRect(0, 0, 50, 50);
-	      //g.drawRect(0, 0, 50, 50);
+			int width = getWidth();
+			int height = getHeight();
+			
+			System.out.println("The screen width is: " + width);
+			System.out.println("The screen height is: " + height);
 	   }
 }
